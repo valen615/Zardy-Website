5912d0b30521be9a256db7372d362a5581f6e5a9
Vatilae <valentinoalmonte2012@gmail.com>
2026-09-11 15:51:03 -0400

    Update monthly listeners count to 100K

--- zardy_website/app.py
+++ zardy_website/app.py
@@ -7,7 +7,7 @@ ARTIST_DATA = {
     "genre": "Indie Rock / Alt Rock / Corecore",
     "spotify_id": "5OylQa5tfrpPvkZLoeHPDh",
     "stats": {
-        "monthly_listeners": "65K+",
+        "monthly_listeners": "100K+",
         "releases": 8,
         "active_years": "2021–2026"
     },
